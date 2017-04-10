b774a11956df7850e72d4859d3fd21fd0e666afd
Victor Yu <victoryu@wireless-10-105-230-69.umd.edu>
2017-04-10 04:15:34 -0400

    Leaving library

--- Assets/Scripts/MoveHarryPotter.cs
+++ Assets/Scripts/MoveHarryPotter.cs
@@ -15,50 +15,59 @@ public class MoveHarryPotter : MonoBehaviour {
 	private int whichAnimation=3;
 	private bool attackingMotion = false;
 
+	private Animator animator;
+
 
 	// Use this for initialization
 	void Start () {
 		spr = GetComponent<SpriteRenderer> ();
+		// TODO can specify folder name in Resources.LoadALL()
 		sprites = Resources.LoadAll<Sprite> ("");
 		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("up") || Input.GetKey (KeyCode.Space)) {
-			spr.sprite = sprites [0];
-		}
-
-		if (Input.GetKey ("left")) {
-			spr.sprite = sprites [2];
-		}
-
-		if (Input.GetKey ("right")) {
-			if(Time.time>=nextUpdate){
-				nextUpdate=(Time.time)+0.20f;
-				if (whichAnimation >= 8) {
-					whichAnimation = 3;
-				}
-				spr.sprite = sprites [whichAnimation];
-				whichAnimation += 1;
-			}
-		}
-
-		// TODO ANIMATE THIS!
-		// TODO INCREMENTALLY GROW THE SMOKE
-		if (Input.GetKeyDown (KeyCode.W)) 
+//		if (Input.GetKey ("up") || Input.GetKey (KeyCode.Space)) {
+//			spr.sprite = sprites [0];
+//		}
+//
+//		if (Input.GetKey ("left")) {
+//			spr.sprite = sprites [2];
+//		}
+//
+//		if (Input.GetKey ("right")) {
+//			if(Time.time>=nextUpdate){
+//				nextUpdate=(Time.time)+0.20f;
+//				if (whichAnimation >= 8) {
+//					whichAnimation = 3;
+//				}
+//				spr.sprite = sprites [whichAnimation];
+//				whichAnimation += 1;
+//			}
+//		}
+//
+//		// TODO ANIMATE THIS!
+//		// TODO INCREMENTALLY GROW THE SMOKE
+		if (Input.GetKeyDown (KeyCode.F)) 
 		{
-			spr.sprite = sprites [8];
-			spr.sprite = sprites [11];
+			animator.SetTrigger ("attackState");
 		}
 	}
 
 	void FixedUpdate () 
 	{
-		var horizontalMove = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		float verticalInput = Input.GetAxis ("Vertical");
+
+		var horizontalMove = new Vector3(horizontalInput, 0, 0);
 		transform.position += horizontalMove * walkingSpeed * Time.deltaTime;
 
-		var verticalMove = new Vector3(0, Input.GetAxis("Vertical"), 0);
+		var verticalMove = new Vector3(0, verticalInput, 0);
 		transform.position += verticalMove * jumpSpeed * Time.deltaTime;
+
+		animator.SetFloat("xInput", horizontalInput);
+		animator.SetFloat("yInput", verticalInput);
 	}
 }
