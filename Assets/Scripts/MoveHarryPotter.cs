e5bb6d1a2c85cf7250cbcbd8fd54f5e76519135f
Victor Yu <victoryu@Victors-MacBook-Pro.local>
2017-05-01 20:32:57 -0400

    Fixed flying animation flaws

--- Assets/Scripts/MoveHarryPotter.cs
+++ Assets/Scripts/MoveHarryPotter.cs
@@ -22,7 +22,7 @@ public class MoveHarryPotter : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D>();
 		animator = GetComponent<Animator> ();
 //		InvokeRepeating ("SpawnDementor", 2.0f, 5.0f);
-//		Invoke ("SpawnDementor", 0.0f);
+		Invoke ("SpawnDementor", 0.0f);
 	}
 	
 	// Update is called once per frame
@@ -42,60 +42,57 @@ public class MoveHarryPotter : MonoBehaviour {
 		if (fellToGroundYet) {
 			float horizontalInput = Input.GetAxis ("Horizontal");
 			float verticalInput = Input.GetAxis ("Vertical");
-
-			RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 2f), Vector2.down);
-			if (this.transform.position.y <= hit.collider.gameObject.transform.position.y + 6f) {
-
-			} else {
-
-			}
-
-			if (horizontalInput != 0 && (verticalInput == 0 || this.transform.position.y <= hit.collider.gameObject.transform.position.y + 6f)) {
-				if (!animator.GetBool ("walkingState")) {
-					animator.SetBool ("walkingState", true);
-					animator.SetBool ("idleState", false);
-				}
-			} else {
-				if (!animator.GetBool ("idleState")) {
-					animator.SetBool ("idleState", true);
-					animator.SetBool ("walkingState", false);
-				}
-			}
-
 			if (Input.GetKey (KeyCode.Space)) {
 				verticalInput = 1f;
 			}
-
 			if (verticalInput == 1) {
 				horizontalInput *= 4;
 			}
 
-			var horizontalMove = new Vector3(horizontalInput, 0, 0);
-			transform.position += horizontalMove * walkingSpeed * Time.deltaTime;
+			RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 2f), Vector2.down);
+			// must be near or on ground
+			if (hit.collider != null && hit.collider.gameObject.tag == "platform"
+				&& this.transform.position.y <= hit.collider.gameObject.transform.position.y + 6f) {
+				if (verticalInput <= 0) {
+					if (animator.GetBool ("flyingState")) {
+						animator.SetBool ("fallingState", true);
+						animator.SetBool ("flyingState", false);
+					}
+					GetComponent<Rigidbody2D> ().gravityScale = 1;
 
-			// NOTE: if you're close to the ground, but somehow there's a dementor or something else underneath you, this won't turn off gravity
-			// only when you're close to the ground AND only the platform is underneath you will the "else" statement be true.
-			if (verticalInput != 0) {
-				if (hit.collider != null && hit.collider.gameObject.tag == "platform"
-					&& this.transform.position.y <= hit.collider.gameObject.transform.position.y + 6f) {
+					if (horizontalInput == 0) {
+						if (!animator.GetBool ("idleState")) {
+							animator.SetBool ("idleState", true);
+							animator.SetBool ("walkingState", false);
+						}
+					} else if (horizontalInput != 0) {
+						if (!animator.GetBool ("walkingState")) {
+							animator.SetBool ("walkingState", true);
+							animator.SetBool ("idleState", false);
+						}
+					}
+				} else if (verticalInput > 0) {
+					if (!animator.GetBool ("idleState")) {
+						animator.SetBool ("idleState", true);
+						animator.SetBool ("walkingState", false);
+					}
 					if (!animator.GetBool ("flyingState")) {
 						animator.SetBool ("flyingState", true);
 						animator.SetBool ("fallingState", false);
 					}
+					GetComponent<Rigidbody2D> ().gravityScale = 0;
 				}
-				GetComponent<Rigidbody2D> ().gravityScale = 0;
-			} else if (hit.collider != null && hit.collider.gameObject.tag == "platform"
-				&& this.transform.position.y <= hit.collider.gameObject.transform.position.y + 6f) {
-				if (animator.GetBool ("flyingState")) {
-					animator.SetBool ("fallingState", true);
-					animator.SetBool ("flyingState", false);
-				}
-				GetComponent<Rigidbody2D> ().gravityScale = 1;
 			} 
+			// must be above ground or flying
+			else if (hit.collider != null && hit.collider.gameObject.tag == "platform") {
 
+			}
+
+			var horizontalMove = new Vector3(horizontalInput, 0, 0);
+			transform.position += horizontalMove * walkingSpeed * Time.deltaTime;
+			// TODO move upwards??
 			var verticalMove = new Vector3(0, verticalInput, 0);
 			transform.position += verticalMove * jumpSpeed * Time.deltaTime;
-
 			animator.SetFloat("xInput", horizontalInput);
 			animator.SetFloat("yInput", verticalInput);
 		}
